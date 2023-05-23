a6242fc4e12f6da7ec5270b8f007fb8117c014a1
Keshri13 <keshri1316@gmail.com>
2023-05-24 01:52:00 +0530

    Second Commit, Project Initiated

---
+++ app/src/main/java/com/example/learnroomdb/StudentDao.kt
@@ -0,0 +1,25 @@
+package com.example.learnroomdb
+
+import androidx.room.*
+
+@Dao
+interface StudentDao {
+
+    @Query("SELECT * FROM student_table")
+    fun getAll():List<Student>
+
+    @Query("SELECT * FROM student_table WHERE roll_no LIKE :roll LIMIT 1")
+    suspend fun findByRoll():Student
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(student: Student)
+
+    @Delete
+    suspend fun delete(student: Student)
+
+    @Query("DELETE FROM student_table")
+    suspend fun deleteAll()
+
+
+
+}
